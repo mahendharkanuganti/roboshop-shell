f5188538054e9b0cf5f740bb9813a59366f65b1b
Mahendhar Kanuganti <kanuganti98@gmail.com>
2024-07-25 10:27:08 +0530

    eksctl creating

--- redis.sh
+++ redis.sh
@@ -30,7 +30,7 @@ fi
 dnf install redis -y &>>$LOGFILE
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' vim /etc/redis/redis.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$LOGFILE
 VALIDATE $? "Remote access to redis server"
 
 systemctl enable redis
